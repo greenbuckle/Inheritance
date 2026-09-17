528b685a3de46d8af8b0a5a8c0523c08b04a76ea
juare <JUAREZJ0303@students.bowiestate.edu>
2026-09-17 10:45:46 -0400

    inheritance-3

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,15 @@ public class Main {
 
 //        Person p2=new Person(75,25,"Black","Barack Obama",170 );
 //        System.out.println(p2.name);
-        Person p2=new Person(75,25 );
+
+        Person p2=new Person(72,70 );
         System.out.println(p2.age);
 
         Person p3=p2.Call_Private();
 
-
-
-
+        Student s1=new Student();
+        s1.age=20;
+        s1.gpa=4.00;
+        System.out.println(s1.gpa);
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,22 @@
+//Student class will inherit Person Class
+//How to inherit: extends
+//How many class can a class inherit: 1
+
+//Student is the child class and Person is the parent class
+//Student is the sub class and Person is the Super Class
+//Person is the base class and Student is the
+
+public class Student extends Person{
+   //Attributes are inherited - Not the private attributes
+    //Setters and Getters are inherited- -  Not the private methods
+    // Methods are inherited - Not the private methods
+
+    //Constructors - Are not inherited, but you can use the constructors
+
+    int student_9d;
+    double gpa;
+
+    Student(){
+
+    }
+}
