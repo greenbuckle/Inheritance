a1ea35c0908c2157cf861a9f4342265b0d3d98ce
juare <JUAREZJ0303@students.bowiestate.edu>
2026-09-17 10:21:57 -0400

    inheritance-2

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,15 @@ public class Main {
         //create two person object
         Person p1=new Person();
 
-        Person p2=new Person(75,25,"Black","Barack Obama",170 );
+//        Person p2=new Person(75,25,"Black","Barack Obama",170 );
+//        System.out.println(p2.name);
+        Person p2=new Person(75,25 );
+        System.out.println(p2.age);
 
-        System.out.println(p2.name);
+        Person p3=p2.Call_Private();
+
+
+
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+//private amd protected are not allowed as class access modules
 public class Person {
     //height,age,color,name,weight
     double height;
@@ -6,7 +7,8 @@ public class Person {
     String name;
     double weight;
 
-    Person(){
+    //Constructors can have private, protected, public access modifiers
+    public Person(){
     //default Constructor can be empty
         height=48;
         age=18;
@@ -15,13 +17,29 @@ public class Person {
         weight=140;
     }
 
+    //Protected members are accessible within the same package - java files under the same directory/folder
+    protected Person(double height, int age){
+        //parameters are local variables to the constructor
+        this.height=height;
+        this.age=age;
+    }
+
     //Overloaded Constructor
-    Person(double height, int age, String color, String name, double weight){
+    private Person(double height, int age, String color, String name, double weight){
         //parameters are local variables to the constructor
         this.height=height;
         this.age=age;
         this.color=color;
         this.name=name;
         this.weight=weight;
+    }
+
+    //Setter and Getter
+    //Methods
+
+    //Return type:Person
+    public Person Call_Private(){
+        Person p3=new Person(72,70,"Black","Barack Obama",178 );
+        return p3;
     }
 }
